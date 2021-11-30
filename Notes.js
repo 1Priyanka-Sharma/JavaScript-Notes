9b5af09e793fc6bedf015710ab1d05032be734e5
Priyanka <priyankaqaz12345@gmail.com>
2021-12-01 01:19:09 +0530

    Asynchonours nature

--- Notes.js
+++ Notes.js
@@ -28,3 +28,30 @@ console.log(obj2);
 // Destructuring objects
 let {name,Hobby,drink}=obj2; 
 console.log(name,Hobby,drink);
+
+// Map Function
+let map_res= a.map((value)=>{
+    return value*10;
+})
+console.log(map_res);
+
+// Filter Function
+let filter_res=a.filter((value)=>{
+    return ((value%2)==0)
+})
+console.log(filter_res);
+
+// Reduce Function
+let reduce_res=a.reduce((value1,value2)=>{
+    return value1+value2
+})
+console.log(reduce_res);
+
+// Triple Equal
+if(1=='1')
+console.log('Without ===; 1=="1" is true');
+
+if(1==='1')
+console.log('Will never execute');
+else
+console.log('With ===; 1=="1" is false');
